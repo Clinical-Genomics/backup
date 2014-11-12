d003b21e8b293d40409d6d6a19404aac8f2b5eb2
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-12 08:49:12 +0100

    Update deletesamplesfromflowcell.py
    added database version check

--- scripts/deletesamplesfromflowcell.py
+++ scripts/deletesamplesfromflowcell.py
@@ -37,8 +37,20 @@ now = time.strftime('%Y-%m-%d %H:%M:%S')
 cnx = mysql.connect(user=params['CLINICALDBUSER'], port=int(params['CLINICALDBPORT']), host=params['CLINICALDBHOST'], 
                     passwd=params['CLINICALDBPASSWD'], db=params['STATSDB'])
 cursor = cnx.cursor()
-
 print ("\n\tFC: "+fcname+"    DATABASE IS "+params['STATSDB']+"\n")
+
+cursor.execute(""" SELECT major, minor, patch FROM version ORDER BY time DESC LIMIT 1 """)
+row = cursor.fetchone()
+if row is not None:
+  major = row[0]
+  minor = row[1]
+  patch = row[2]
+else:
+  sys.exit("Incorrect DB, version not found.")
+if (major == _MAJOR_ and minor == _MINOR_ and patch == _PATCH_):
+  print "Correct database "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)
+else:
+  exit ("Incorrect DB version. This script is made for "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)+" not for "+str(major)+"."+str(minor)+"."+str(patch))
 
 cursor.execute(""" SELECT project.projectname, flowcell.flowcellname, sample.samplename, unaligned.lane, 
 unaligned.readcounts, unaligned.yield_mb, TRUNCATE(q30_bases_pct,2), TRUNCATE(mean_quality_score,2),
