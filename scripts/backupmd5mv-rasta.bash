3eb048394a4f89e7a52919b998b08aef17a50f52
kenny billiau <kenny.billiau@gmail.com>
2015-12-20 20:09:53 +0100

    Fix backup rasta mv to oldRuns

--- scripts/backupmd5mv-rasta.bash
+++ scripts/backupmd5mv-rasta.bash
@@ -3,14 +3,17 @@
 VERSION=2.0.1
 echo "VERSION ${VERSION}"
 
+# exit on empty var (to avoid to mv an empty ${RUN}
+set -u
+
 RUNDIR=/mnt/hds2/proj/bioinfo/Runs/
 OLDRUNDIR=/mnt/hds2/proj/bioinfo/oldRuns/
 BACKUPDIR=/mnt/hds/proj/bioinfo/BACKUP/
 NASRUNDIR=/home/hiseq.clinical/Runs/
 NASOLDRUNDIR=/home/hiseq.clinical/oldRuns/
 
-runs=$(find ${RUNDIR} -maxdepth 1 -mtime +15 | awk 'BEGIN {FS="/"} {print $NF}')
-for RUN in $runs; do
+RUNS=$(find ${RUNDIR} -maxdepth 1 -mtime +15 | awk 'BEGIN {FS="/"} {print $NF}')
+for RUN in ${RUNS}; do
     echo ${BACKUPDIR}/${RUN}.tar.gz
 
     echo tar -czf ${BACKUPDIR}/${RUN}.tar.gz ${RUNDIR}/${RUN}/
@@ -23,20 +26,20 @@ for RUN in $runs; do
     mv ${RUNDIR}/${RUN} ${OLDRUNDIR}/
 
     for NAS in seq-nas-2 nas-7 nas-8 nas-9 nas-10; do
-        ssh ${NAS} "ls ${NASRUNDIR}/${run}"
+        ssh ${NAS} "ls ${NASRUNDIR}/${RUN}"
         sshcommand=$?
         if [[ ${sshcommand} != 0 ]] ; then
             echo "skipping ${NAS}..."
 	    continue # it's not on this NAS
         fi
 
-        ssh ${NAS} "mv ${NASRUNDIR}/${run} ${NASOLDRUNDIR}"
+        ssh ${NAS} "mv ${NASRUNDIR}/${RUN} ${NASOLDRUNDIR}"
         sshcommand=$?
         NOW=$(date +"%Y%m%d%H%M%S")
         if [[ ${sshcommand} != 0 ]] ; then
-            echo "ssh ${NAS} mv ${NASRUNDIR}${run} ${NASOLDRUNDIR} failed:${sshcommand}"
+            echo "ssh ${NAS} mv ${NASRUNDIR}${RUN} ${NASOLDRUNDIR} failed:${sshcommand}"
         else
-            echo "ssh ${NAS} mv ${NASRUNDIR}${run} ${NASOLDRUNDIR} completed"
+            echo "ssh ${NAS} mv ${NASRUNDIR}${RUN} ${NASOLDRUNDIR} completed"
         fi
     done
 done
