66658fb0cb7458a0968dd2c1477b6bf10075befd
kenny billiau <kenny.billiau@scilifelab.se>
2016-04-26 10:51:52 +0200

    Feat backup all databases on clinical-db

--- scripts/clinstatsdb_backup.bash
+++ scripts/clinstatsdb_backup.bash
@@ -12,13 +12,43 @@
 ## host = localhost
 #
 
+set -e
+
+##########
+# CONFIG #
+##########
+
 VERSION=3.2.2
 echo "VERSION ${VERSION}"
 
 . /home/clinical/CONFIG/configuration.txt
-NOW=$(date +"%Y%m%d%H%M%S")
-OUTFILE=${BACKUPDIR}clinical-db_${NOW}.sql
-mysqldump --databases csdb nipt_new > $OUTFILE
 
-scp $OUTFILE rasta:/mnt/hds/proj/bioinfo/BACKUP/clinstatsdb_${NOW}.sql
+#########
+# TRAPS #
+#########
 
+errr() {
+    echo "Error while backing up ${DATABASE}" | mail -s "Error while backing up ${DATABASE}"
+}
+trap errr ERR 
+
+########
+# MAIN #
+########
+
+DATABASES=$(mysql --skip-column-names -e 'show databases')
+
+for DATABASE in $DATABASES; do
+    if echo "$DATABASE" | egrep -qs '^(csdb|nipt_.*)$'; then
+        echo "Backup up $DATABASE"
+    else
+        echo "Not backing up $DATABASE"
+        continue
+    fi
+    
+    NOW=$(date +"%Y%m%d%H%M%S")
+    OUTFILE=${BACKUPDIR}/${DATABASE}_${NOW}.sql.gz
+    mysqldump $DATABASE | gzip -9 > $OUTFILE
+
+    scp $OUTFILE rasta:/mnt/hds/proj/bioinfo/BACKUP/
+done
