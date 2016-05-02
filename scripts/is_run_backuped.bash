0eea28e60aa355d3af7f6badd41f0fe78c872318
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-05-02 14:44:48 +0200

    Refactor is_run_backuped to list missing runs

--- scripts/is_run_backuped.bash
+++ scripts/is_run_backuped.bash
@@ -10,48 +10,44 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 RESET="\033[0m"
 
-while read -a REPLY; do
-    SIZE=${REPLY[0]};
-    RUN=${REPLY[1]};
-
-    if [[ -z ${RUN} ]]; then
-        continue
-    fi
-
-    if [[ ${RUN} == *.key.gpg ]]; then
-        continue
-    fi
-
-    SIZE=${SIZE//,}
+for RUN in ${BACKUP_DIR}/*; do
     RUN=${RUN##*/}
-    RUN=${RUN%%.*}
-    if [[ -e ${BACKUP_DIR}/${RUN} ]]; then
-        # check size
-        if grep -qs Description,NIPTv1 ${BACKUP_DIR}/${RUN}/SampleSheet.csv; then # NIPT RUN
-            EXPECTED_SIZE=9000000 # 9GB
-        elif [[ ${RUN} == *CCXX ]]; then
-            EXPECTED_SIZE=580000000 # 550GB
-        else
-            EXPECTED_SIZE=35000000 # 35GB
-        fi
 
-        
+    # is the run on PDC?
+    read -a RUN_SIZE <<< $(grep ${RUN}.tar.gz.gpg ${IN_FILE})
+    if [[ ${#RUN_SIZE[@]} -ne 2 ]]; then
+        echo -e "${RED}${RUN}${RESET}\t"
+        continue
+    else
         echo -en "${RUN}\t"
-        if [[ $SIZE -lt $EXPECTED_SIZE ]]; then
-            echo -en "${RED}${SIZE}${RESET}\t"
-        else
-            echo -en "${GREEN}${SIZE}${RESET}\t"
-        fi
+    fi
 
-        read -a KEY <<< $(grep ${RUN}.key.gpg ${IN_FILE})
-        if [[ ${#KEY[@]} -ne 2 ]]; then
-            echo -e "${RED}MISSING KEY${RESET}"
+    # check size
+    SIZE=${RUN_SIZE[0]};
+    SIZE=${SIZE//,}
+    if grep -qs Description,NIPTv1 ${BACKUP_DIR}/${RUN}/SampleSheet.csv; then # NIPT RUN
+        EXPECTED_SIZE=9000000 # 9GB
+    elif [[ ${RUN} == *CCXX ]]; then
+        EXPECTED_SIZE=580000000 # 550GB
+    else
+        EXPECTED_SIZE=35000000 # 35GB
+    fi
+
+    if [[ $SIZE -lt $EXPECTED_SIZE ]]; then
+        echo -en "${RED}${SIZE}${RESET}\t"
+    else
+        echo -en "${GREEN}${SIZE}${RESET}\t"
+    fi
+
+    # check the key
+    read -a KEY <<< $(grep ${RUN}.key.gpg ${IN_FILE})
+    if [[ ${#KEY[@]} -ne 2 ]]; then
+        echo -e "${RED}MISSING KEY${RESET}"
+    else
+        if [[ ${KEY[0]} -ne 607 ]]; then
+            echo -e "${RED}KEY WRONG SIZE ${KEY[0]}${RESET}"
         else
-            if [[ ${KEY[0]} -ne 607 ]]; then
-                echo -e "${RED}KEY WRONG SIZE ${KEY[0]}${RESET}"
-            else
-                echo -e "${GREEN}KEY FOUND${RESET}"
-            fi
+            echo -e "${GREEN}KEY FOUND${RESET}"
         fi
     fi
-done < ${IN_FILE}
+done
