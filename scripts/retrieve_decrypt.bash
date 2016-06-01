d96896788cfe64d4fdc82e54356aa33791cb56ce
kenny billiau <kenny.billiau@gmail.com>
2016-06-01 13:21:02 +0200

    Add retrieve_decrypt.bash

---
+++ scripts/retrieve_decrypt.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# retrieves and transfers a file from PDC
+
+set -e
+
+########
+# VARS #
+########
+
+RESTORE_FILE=$1
+DEST_SERVER=$2
+DEST_DIR=$3
+
+if [[ ${#@} -ne 3 ]]; then
+    >&2 echo -e "USAGE:\n\t$0 source_filename server dest_filename"
+    exit 1
+fi
+
+RUNDIR=$(dirname $RESTORE_FILE)
+RUN=$(basename $RESTORE_FILE)
+RUN=${RUN%%.*}
+
+#############
+# FUNCTIONS #
+#############
+
+log() {
+    NOW=$(date +"%Y%m%d%H%M%S")
+    echo "[${NOW}] $@"
+}
+
+finish() {
+    if [[ -e $FIFO ]]; then
+        rm $FIFO
+    fi
+}
+
+#########
+# TRAPS #
+#########
+
+trap finish EXIT ERR
+
+########
+# MAIN #
+########
+
+read -s -p "Passphrase: " PASSPHRASE
+
+# get the encrypted key first
+KEY_FILE=$(mktemp -u)
+log "dsmc retrieve '${RUN}.key.gpg' $KEY_FILE"
+dsmc retrieve "${RUN}.key.gpg" $KEY_FILE
+
+# create named pipe
+FIFO=$(mktemp -u)
+mkfifo $FIFO
+
+# init the tunnel
+cat $FIFO | gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase "$PASSPHRASE" --batch --decrypt ${KEY_FILE}) --batch --decrypt | ssh $DEST_SERVER "cd ${DEST_DIR} && tar xzf -" &
+
+# retrieve the backup
+log "dsmc retrieve '$RESTORE_FILE' $FIFO"
+dsmc retrieve "$RESTORE_FILE" $FIFO
