e106b0279b904fe216bfc66d0b661fdcc1dc743d
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-08-19 11:20:57 +0200

    fix typo

--- scripts/retrieve_decrypt_safe_mode.bash
+++ scripts/retrieve_decrypt_safe_mode.bash
@@ -145,7 +145,7 @@ fi
 trap "remove_file '${DECRYPTED_FILE}'; error" ERR
 if [[ ! -e ${DECRYPTED_FILE} ]]; then
   log "gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase <NOT-SHOWN> --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}"
-  gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase "'${$PASSPHRASE}'" --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}
+  gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase "'${PASSPHRASE}'" --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}
 else
   log "Found decrypted run file '${DECRYPTED_FILE}', skipping gpg decrypting run"
 fi
