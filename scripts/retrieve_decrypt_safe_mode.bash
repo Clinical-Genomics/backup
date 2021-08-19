2cda83ca6a86c589ea00378b89f8f78cedaa3d60
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-08-19 09:23:52 +0200

    replace overwrite confirm with skip

--- scripts/retrieve_decrypt_safe_mode.bash
+++ scripts/retrieve_decrypt_safe_mode.bash
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -Ex
+set -E
 
 ########
 # VARS #
@@ -44,60 +44,70 @@ log_exc() {
 }
 
 cleanup() {
-    # remove key
-    if [[ -e $KEY_FILE ]]; then
-        log "removing key $KEY_FILE"
-        log_exc "rm $KEY_FILE"
-    fi
-    # remove retrieved file
-    if [[ -e ${RETRIEVED_FILE} ]]; then
-        log "removing retrieved file ${RETRIEVED_FILE}"
-        log_exc "rm ${RETRIEVED_FILE}"
-    fi
 
-    # remove decrypted file
-    if [[ -e ${DECRYPTED_FILE} ]]; then
-        log "removing decrypted file ${DECRYPTED_FILE}"
-        log_exc "rm ${DECRYPTED_FILE}"
-    fi
+    log "Removing files in temporary folder '${TMP_DIR}':"
+    log_exc "cd ${TMP_DIR}"
+    list_files
 
-    # remove decompressed folder
-    if [[ -d ${RUN_NAME} ]]; then
-        log "removing decompressed folder ${RUN_NAME}"
-        log_exc "rm -rf ${RUN_NAME}"
-    fi
+    log "removing dsmc error log dsmerror.log"
+    log_exc "remove_file dsmerror.log"
 
-    # temporary folder
-    cd ${START_DIR}
-    if [[ -d ${TMP_DIR} ]]; then
-        log "removing temporary folder ${TMP_DIR}"
-        log_exc "rm -rf ${TMP_DIR}"
-    fi
+    log "removing key $KEY_FILE"
+    log_exc "remove_file $KEY_FILE"
 
+    log "removing retrieved run file ${RETRIEVED_FILE}"
+    log_exc "remove_file ${RETRIEVED_FILE}"
+
+    log "removing decrypted file ${DECRYPTED_FILE}"
+    log_exc "remove_file ${DECRYPTED_FILE}"
+
+    log "removing decompressed folder ${RUN_NAME}"
+    log_exc "remove_folder_recursive ${RUN_NAME}"
+
+    log_exc "remove_empty_folder ${START_DIR}/${TMP_DIR}"
     log "finished!"
+}
+
+show_dsmc_errors() {
+  log_exc "tail dsmerror.log"
+}
+
+remove_file() {
+  TO_REMOVE=$1
+  if [[ -e ${TO_REMOVE} ]]; then
+      log "Removing file ${TO_REMOVE}"
+      log "rm -I ${TO_REMOVE}"
+  fi
+}
+
+remove_empty_folder() {
+  TO_REMOVE=$1
+  if [[ -e ${TO_REMOVE} ]]; then
+      log "Removing empty folder ${TO_REMOVE}"
+      log "rmdir ${TO_REMOVE}"
+  fi
+}
+
+remove_folder_recursive() {
+  TO_REMOVE=$1
+  if [[ -e ${TO_REMOVE} ]]; then
+      log "Removing folder recursively ${TO_REMOVE}"
+      log "rm -rf ${TO_REMOVE}"
+  fi
 }
 
 error() {
   log "Something went wrong! leaving files for manual handling:"
+  list_files
+}
+
+list_files() {
   for entry in "$START_DIR/$TMP_DIR"/*
   do
-    echo "$entry"
+    log "$entry"
   done
 }
 
-not_exists_or_confirm_overwrite() {
-  if [ ! -e "$1" ]
-  then
-    return 1
-  else
-    read -p "`echo $'\n '`File $1 already exist, Overwrite? " -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-      return 1
-    else
-      return 0
-    fi
-  fi
 }
 
 #########
@@ -116,54 +126,52 @@ if [[ ! -d ${TMP_DIR} ]]; then
 fi
 log_exc "cd ${TMP_DIR}"
 
-# get the encrypted key first
+# STEP 1: get the encrypted key
 # if not exists or confirm_overwrite
-if [[ $(not_exists_or_confirm_overwrite ${KEY_FILE}) ]]; then
+trap "remove_file '${KEY_FILE}'; error" ERR
+if [[ ! -e ${KEY_FILE} ]]; then
   log_exc "dsmc retrieve -replace=yes '${RUN_DIR}/${RUN_NAME}.key.gpg' '${KEY_FILE}'"
 else
-  log "skipping retrieving key"
+  log "Found key file '${KEY_FILE}', skipping retrieving key"
 fi
 
-# retrieve run
-if [[ $(not_exists_or_confirm_overwrite ${RETRIEVED_FILE}) ]]; then
+# STEP 2: retrieve run
+trap "remove_file '${RETRIEVED_FILE}'; error" ERR
+if [[ ! -e ${RETRIEVED_FILE} ]]; then
   log_exc "dsmc retrieve -replace=yes '${RESTORE_FILE}' '${RETRIEVED_FILE}'"
 else
-  log "skipping retrieving run"
+  log "Found run file '${RETRIEVED_FILE}', skipping retrieving run"
 fi
 
-# decrypt run
-if [[ $(not_exists_or_confirm_overwrite ${DECRYPTED_FILE}) ]]; then
+# STEP 3: decrypt run
+trap "remove_file '${DECRYPTED_FILE}'; error" ERR
+if [[ ! -e ${DECRYPTED_FILE} ]]; then
   log_exc "time gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase '${PASSPHRASE}' --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}"
 else
-  log "skipping gpg"
+  log "Found decrypted run file '${DECRYPTED_FILE}', skipping gpg decrypting run"
 fi
 
-# decompress run
-if [[ $(not_exists_or_confirm_overwrite ${RUN_NAME}) ]]; then
+# STEP 4: decompress run
+trap "remove_folder '${RUN_NAME}'; error" ERR
+if [[ ! -e ${RUN_NAME} ]]; then
   log_exc "time tar xf ${DECRYPTED_FILE} --exclude='RTAComplete.txt' --exclude='demuxstarted.txt' --exclude='Thumbnail_Images'"
 else
-  log "skipping untaring"
+  log "Found decompressed run folder '${RUN_NAME}', skipping decompressing run"
 fi
 
 if [[ ${DEST_SERVER} == 'localhost' ]]; then
-  # rsync run
-  if [[ $(not_exists_or_confirm_overwrite ${DEST_DIR}) ]]; then
-    log_exc "time rsync -r --progress ${RUN_NAME} ${DEST_DIR} --partial-dir=${DEST_DIR}.partial --delay-updates"
-  else
-    log "skipping rsync"
-  fi
 
-  # mark as finished
+  # STEP 5: rsync run
+  trap error ERR
+  log_exc "time rsync -r --progress ${RUN_NAME} ${DEST_DIR} --partial-dir=${DEST_DIR}.partial --delay-updates"
+
+  # STEP 6: mark as finished
   log_exc "touch ${DEST_DIR}/${RUN_NAME}/RTAComplete.txt"
 else
-  # rsync run
-  if [[ $(not_exists_or_confirm_overwrite ${DEST_DIR}) ]]; then
-    log_exc "time rsync -r --progress ${RUN_NAME} hiseq.clinical@$DEST_SERVER:${DEST_DIR} --partial-dir=${DEST_DIR}.partial --delay-updates"
-  else
-    log "skipping rsync"
-  fi
+  # STEP 5: rsync run
+  log_exc "time rsync -r --progress ${RUN_NAME} hiseq.clinical@$DEST_SERVER:${DEST_DIR} --partial-dir=${DEST_DIR}.partial --delay-updates"
 
-  # mark as finished
+  # STEP 6: mark as finished
   log_exc "ssh $DEST_SERVER 'touch ${DEST_DIR}/${RUN_NAME}/RTAComplete.txt'"
 fi
 
