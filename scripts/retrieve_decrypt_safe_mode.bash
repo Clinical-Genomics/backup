d3041616ef13aacf736b581f3586344ef14f0a40
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-10-28 13:47:38 +0200

    Feat semaphore safe retrieve (#25) (patch)
    ### Fixed:
    - Double invocation should quit the second invocation

--- scripts/retrieve_decrypt_safe_mode.bash
+++ scripts/retrieve_decrypt_safe_mode.bash
@@ -27,6 +27,7 @@ TMP_DIR="${RUN_NAME}.TMP"
 KEY_FILE="${RUN_NAME}.key.gpg"
 RETRIEVED_FILE="./${RUN_FILE}"
 DECRYPTED_FILE="${RUN_FILE%%.gpg}"
+SEMAPHORE="running"
 
 #############
 # FUNCTIONS #
@@ -63,6 +64,9 @@ cleanup() {
     log "removing decompressed folder ${RUN_NAME}"
     log_exc remove_folder_recursive ${RUN_NAME}
 
+    log "removing semaphore file " ${SEMAPHORE}
+    log_exc remove_file ${SEMAPHORE}
+
     log_exc remove_empty_folder ${START_DIR}/${TMP_DIR}
     log "finished!"
 }
@@ -98,6 +102,7 @@ remove_folder_recursive() {
 error() {
   log "Something went wrong! leaving files for manual handling:"
   list_files
+  remove_file ${SEMAPHORE}
 }
 
 list_files() {
@@ -107,13 +112,7 @@ list_files() {
   done
 }
 
-#########
-# TRAPS #
-#########
-
-trap error ERR
-
-########
+#######
 # MAIN #
 ########
 
@@ -123,36 +122,60 @@ if [[ ! -d ${TMP_DIR} ]]; then
 fi
 log_exc cd ${TMP_DIR}
 
+MAX_AGE=720
+if [[ ! -f ${SEMAPHORE} ]]; then
+  log_exc touch ${SEMAPHORE}
+elif [[ $(find ${SEMAPHORE} -maxdepth 0 -cmin -${MAX_AGE}) ]]; then
+  log "Semaphore file '${TMP_DIR}/${SEMAPHORE}' exists. Fetching of flowcell already in progress. Quiting"
+  exit 0
+else
+  log "Semaphore file '${TMP_DIR}/${SEMAPHORE}' exists but is older than ${MAX_AGE} minutes, quiting with error status"
+  exit 1
+fi
+
 # STEP 1: get the encrypted key
 # if not exists or confirm_overwrite
-trap "remove_file ${KEY_FILE}; error" ERR
+TMP_KEY=${KEY_FILE}.tmp
+trap "remove_file ${TMP_KEY}; error" ERR
 if [[ ! -f ${KEY_FILE} ]]; then
-  log_exc dsmc retrieve -replace=yes ${RUN_DIR}/${RUN_NAME}.key.gpg ${KEY_FILE}
+  log_exc dsmc retrieve -replace=yes ${RUN_DIR}/${RUN_NAME}.key.gpg ${TMP_KEY}
+  mv ${TMP_KEY} ${KEY_FILE}
 else
   log "Found key file ${KEY_FILE}, skipping retrieving key"
 fi
 
 # STEP 2: retrieve run
-trap "remove_file ${RETRIEVED_FILE}; error" ERR
+TMP_RETRIEVED_FILE=${RETRIEVED_FILE}.tmp
+trap "remove_file ${TMP_RETRIEVED_FILE}; error" ERR
 if [[ ! -f ${RETRIEVED_FILE} ]]; then
-  log_exc dsmc retrieve -replace=yes ${RESTORE_FILE} ${RETRIEVED_FILE}
+  log_exc dsmc retrieve -replace=yes ${RESTORE_FILE} ${TMP_RETRIEVED_FILE}
+  mv ${TMP_RETRIEVED_FILE} ${RETRIEVED_FILE}
 else
   log "Found run file ${RETRIEVED_FILE}, skipping retrieving run"
 fi
 
 # STEP 3: decrypt run
-trap "remove_file ${DECRYPTED_FILE}; error" ERR
+TMP_DECRYPTED_FILE=${DECRYPTED_FILE}.tmp
+trap "remove_file ${TMP_DECRYPTED_FILE}; error" ERR
 if [[ ! -f ${DECRYPTED_FILE} ]]; then
-  log "gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase <NOT-SHOWN> --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}"
-  gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase "${PASSPHRASE}" --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${DECRYPTED_FILE}
+  log "gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase <NOT-SHOWN> --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${TMP_DECRYPTED_FILE}"
+  gpg --cipher-algo aes256 --passphrase-file <(gpg --cipher-algo aes256 --passphrase "${PASSPHRASE}" --batch --decrypt ${KEY_FILE}) --batch --decrypt ${RUN_FILE} > ${TMP_DECRYPTED_FILE}
+  mv ${TMP_DECRYPTED_FILE} ${DECRYPTED_FILE}
 else
   log "Found decrypted run file ${DECRYPTED_FILE}, skipping gpg decrypting run"
 fi
 
 # STEP 4: decompress run
-trap "remove_folder_recursive ${RUN_NAME}; error" ERR
+TMP_RUN_NAME=${RUN_NAME}.tmp
+trap "remove_folder_recursive ${TMP_RUN_NAME}/${RUN_NAME}; error" ERR
 if [[ ! -d ${RUN_NAME} ]]; then
-  log_exc tar xf ${DECRYPTED_FILE} --exclude=RTAComplete.txt --exclude=demuxstarted.txt --exclude=Thumbnail_Images
+  if [[ ! -d ${TMP_RUN_NAME} ]]; then
+    log_exc mkdir ${TMP_RUN_NAME}
+  fi
+
+  log_exc tar -xf ${DECRYPTED_FILE} --exclude=RTAComplete.txt --exclude=demuxstarted.txt --exclude=Thumbnail_Images -C ${TMP_RUN_NAME}
+  mv ${TMP_RUN_NAME}/${RUN_NAME} ${RUN_NAME}
+  remove_empty_folder ${TMP_RUN_NAME}
 else
   log "Found decompressed run folder ${RUN_NAME}, skipping decompressing run"
 fi
